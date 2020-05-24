c60b9fab92b16ba6d862954ae6877c3a0165ddf7
Nicolas Brignone <nmbrignone@gmail.com>
2020-05-24 20:23:58 -0300

    added pid thermal controller?!

--- influx/influx_loader.py
+++ influx/influx_loader.py
@@ -9,6 +9,7 @@ import time
 import sys
 import argparse
 import subprocess
+import simple_pid
 
 sensors = {
     b"uno":0.0,
@@ -28,9 +29,6 @@ class Reader(threading.Thread):
         #self.serConf() # Uncomment lines here till it works
 
         self.ser.open()
-
-        #self.addr = None
-        #self.setAddress(0)
 
     def update_sensor(self,l):
         for s in sensors.keys():
@@ -87,6 +85,23 @@ class Reader(threading.Thread):
     def close(self):
         self.ser.close()
 
+class ReaderMock(threading.Thread):
+
+    def __init__(self):
+        super(ReaderMock, self).__init__(daemon=True)
+
+    def some_value(self, sensor):
+        return 1.23
+
+    def update_sensors(self):
+        for s in sensors.keys():
+            sensors[s] = self.some_value(s)
+            #print("updating sensor: {}:{}".format(s,sensors[s]))
+
+    def run(self):
+        while(True):
+            time.sleep(1)
+            self.update_sensors()
 
 
 class InfluxLoader(threading.Thread):
@@ -99,9 +114,9 @@ class InfluxLoader(threading.Thread):
         port=8086
         self.client = InfluxDBClient(host, port, USER, PASSWORD, DBNAME)
 
-        self.retention_policy = 'awesome_policy'
+        self.retention_policy = '30d_awesome_policy'
         self.s = sensor
-        #self.client.create_retention_policy(self.retention_policy, '3d', 3, default=True)
+        self.client.create_retention_policy(self.retention_policy, '30d', 3, default=True)
 
     def load_temp_to_db(self):
         value = sensors[self.s]
@@ -120,8 +135,9 @@ class InfluxLoader(threading.Thread):
         self.client.write_points(series, retention_policy=self.retention_policy)
 
     def now(self):
-        print("today = {}".format(datetime.datetime.today()))
-        return datetime.datetime.today()
+        _now = datetime.datetime.now(datetime.timezone.utc)
+        print("today = {}".format(_now))
+        return _now
 
     def run(self):
         while True:
@@ -190,13 +206,52 @@ class ThermalController(threading.Thread):
 
             time.sleep(2)
 
+class PIDThermalController(ThermalController):
+    def __init__(self, *args, kp, ki, kd, sample_time, pwm_period_s, **kwargs):
+        #PID(Kp=1.0, Ki=0.0, Kd=0.0, setpoint=0, sample_time=0.01, output_limits=(None, None), auto_mode=True, proportional_on_measurement=False)
+        super(PIDThermalController, self).__init__(*args, **kwargs)
+        self.pid = simple_pid.PID(kp, ki, kd, setpoint=self.max,
+                                  output_limits=(0, pwm_period_s))
+        self.pid.sample_time = sample_time
+        self.pwm_period_s = pwm_period_s
+        #max output is pwm_period_s
+        #pid = PID(1, 0.1, 0.05, setpoint=1)
+
+
+    def run(self):
+        ctime=0
+        step = 4
+        while True:
+            time.sleep(step)
+            ctime = ctime + step
+            if ctime > self.pwm_period_s:
+                ctime = 0
+
+            output = self.pid(sensors[self.s])
+            print("output = ", output)
+            print("ctime = ", ctime)
+            print("pwm_period_s = ", self.pwm_period_s)
+            print("sample_time = ", self.pid.sample_time)
+
+            if ctime < output:
+                self.heater_on()
+            else:
+                self.heater_off()
+
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--mock-reader', action='store_true')
     parser.add_argument('--use-real-heater', action='store_true')
     parser.add_argument('--use-serial', action='store_true')
     parser.add_argument("-t", "--temp", type=float, required=True, help="control temperature")
+    parser.add_argument('--use-pid', action='store_true')
+    parser.add_argument("--sample-time", type=float, default= 60.0, help="sample_time")
+    parser.add_argument("--pwm-period", type=float, default= 30.0, help="pwm-period in seconds")
+    parser.add_argument("-kp", type=float, default= 1.0,  help="Kp")
+    parser.add_argument("-ki", type=float, default= 0.1, help="Ki")
+    parser.add_argument("-kd", type=float, default= 0.05, help="Kd")
     parser.add_argument("-s", "--sensor",
                         choices=["uno","dos","tres","cuatro","cinco","seis"],
                         required=True,
@@ -207,18 +262,34 @@ if __name__ == "__main__":
 
     args=parser.parse_args()
 
-    r = Reader(tty_name=args.port)
+    if args.mock_reader:
+        r=ReaderMock()
+    else:
+        r = Reader(tty_name=args.port)
     r.start()
 
     fp = FilePrinter()
     fp.start()
 
-    tc=ThermalController( temp=args.temp,
-                         sensor=args.sensor.encode("utf-8"),
-                         use_real_heater=args.use_real_heater,
-                         use_serial=args.use_serial,
-                         reader=r,
-    )
+    controller_args={"temp":args.temp,
+                     "sensor": args.sensor.encode("utf-8"),
+                     "use_real_heater":args.use_real_heater,
+                     "use_serial":args.use_serial,
+                     "reader":r,
+                    }
+
+    if args.use_pid:
+        tc=PIDThermalController(
+                            kp=args.kp,
+                            ki=args.ki,
+                            kd=args.kd,
+                            sample_time=args.sample_time,
+                            pwm_period_s=args.pwm_period,
+                            **controller_args,
+        )
+    else:
+        tc=ThermalController(**controller_args)
+
     tc.start()
 
     il=InfluxLoader(sensor=args.sensor.encode("utf-8"))
